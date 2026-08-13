127270526df7fea99c0714711feed871f97f4560
theirrationalone <anilkushwaha289@gmail.com>
2026-08-13 23:18:34 +0000

    assembly-math: Implement all arithmetic operations

--- assembly-math/src/AssemblyMath.sol
+++ assembly-math/src/AssemblyMath.sol
@@ -8,4 +8,28 @@ contract AssemblyMath {
             result := add(a, b)
         }
     }
+
+    function subtract(uint256 a, uint256 b) external pure returns (uint256 result) {
+        assembly {
+            result := sub(a, b)
+        }
+    }
+
+    function multiply(uint256 a, uint256 b) external pure returns (uint256 result) {
+        assembly {
+            result := mul(a, b)
+        }
+    }
+
+    function divide(uint256 a, uint256 b) external pure returns (uint256 result) {
+        assembly {
+            result := div(a, b)
+        }
+    }
+
+    function modulus(uint256 a, uint256 b) external pure returns (uint256 result) {
+        assembly {
+            result := mod(a, b)
+        }
+    }
 }
