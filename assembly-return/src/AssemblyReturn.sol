4404a7d147f647cb1178065ae68009e9270f588c
theirrationalone <anilkushwaha289@gmail.com>
2026-08-28 02:13:19 +0000

    AssemblyReturn setup ready

---
+++ assembly-return/src/AssemblyReturn.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.30;
+
+contract AssemblyReturn {
+    // @TODO: Implement "from memory return usecase"
+
+    }
---
+++ assembly-return/test/AssemblyReturn.t.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.30;
+
+import {Test} from "forge-std/Test.sol";
+import {AssemblyReturn} from "../src/AssemblyReturn.sol";
+
+contract AssemblyReturnTest is Test {
+    // @TODO: Test: AssemblyReturn.sol::AssemblyReturn
+
+    }
+
