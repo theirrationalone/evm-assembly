b940e4d40731a818f711e8a8a9fba87ca692c12a
theirrationalone <anilkushwaha289@gmail.com>
2026-08-28 03:41:19 +0000

    AssemblyReturn finished✅

--- assembly-return/src/AssemblyReturn.sol
+++ assembly-return/src/AssemblyReturn.sol
@@ -3,6 +3,23 @@
 pragma solidity ^0.8.30;
 
 contract AssemblyReturn {
-    // @TODO: Implement "from memory return usecase"
+    function double(uint256 x) external pure returns (uint256) {
+        assembly {
+            x := mul(x, 2)
+            // Answer1: mstore: It picks up the x (not the identifer itself but the value contained into it) stores it to the memory with the offset(the distance is 0 to reach data) 0
+            // Padding comes a makes the fit into 32-byte word. Data x captures the 32 bytes and resides into memory with offset 0x00 (0)
+            // i.e. memory: [0x00:0x20] -> 0x00000000000000000000000000000000000000000000000000000000000000xx (for data with value under 255)
+            mstore(0x00, x)
 
+            // Answer2: As it is known that the memory holds 32-bytes words and [0x00:0x1f] contains our data and that's actually 32-bytes
+            // That's why we choose 0x20 to return 32 bytes starting with offset 0x00
+            return(0x00, 0x20)
+        }
+
+        // Answer3: mstore(0x00, 42), return(0x00, 0x40) -> In that case function would return 64 bytes of data and our test would get failed
+        // Because 64bytes would contain: 0x000000000000000000000000000000000000000000000000000000000000002a0000000000000000000000000000000000000000000000000000000000000000 and is way more than type(uint256).max
+        // So wrap around value would be spitted out (could be accidently correct though)
+
+        // Answer4: mstore(0x20, 42), return(0x00, 0x20) -> data would get into obscurity and return value would be 0x0000000000000000000000000000000000000000000000000000000000000000 (it could be something else already stored at 0x00-0x1f)
     }
+}
--- assembly-return/test/AssemblyReturn.t.sol
+++ assembly-return/test/AssemblyReturn.t.sol
@@ -6,7 +6,69 @@ import {Test} from "forge-std/Test.sol";
 import {AssemblyReturn} from "../src/AssemblyReturn.sol";
 
 contract AssemblyReturnTest is Test {
-    // @TODO: Test: AssemblyReturn.sol::AssemblyReturn
+    AssemblyReturn private asmReturn;
 
+    address OWNER = makeAddr("OWNER");
+
+    function setUp() external {
+        vm.startPrank(OWNER);
+        asmReturn = new AssemblyReturn();
+        vm.stopPrank();
     }
+
+    function test_doubleCase1() public view {
+        uint256 x = 0;
+
+        uint256 double = asmReturn.double(x);
+        assertEq(double, 0);
+    }
+
+    function test_doubleCase2() public view {
+        uint256 x = 1;
+        uint256 expected;
+
+        unchecked {
+            expected = x * 2;
+        }
+
+        uint256 double = asmReturn.double(x);
+        assertEq(double, expected);
+    }
+
+    function test_doubleCase3() public view {
+        uint256 x = 21;
+        uint256 expected;
+
+        unchecked {
+            expected = x * 2;
+        }
+
+        uint256 double = asmReturn.double(x);
+        assertEq(double, expected);
+    }
+
+    function test_doubleCase4() public view {
+        uint256 x = 100;
+        uint256 expected;
+
+        unchecked {
+            expected = x * 2;
+        }
+
+        uint256 double = asmReturn.double(x);
+        assertEq(double, expected);
+    }
+
+    function test_doubleCase5() public view {
+        uint256 x = type(uint256).max;
+        uint256 expected;
+
+        unchecked {
+            expected = x * 2;
+        }
+
+        uint256 double = asmReturn.double(x);
+        assertEq(double, expected);
+    }
+}
 
