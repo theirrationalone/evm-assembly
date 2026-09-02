b6ebf264952634cb06fd36bd2a57b843c23ba7b5
theirrationalone <anilkushwaha289@gmail.com>
2026-09-02 10:56:51 +0000

    assembly calldata dynamic finished✅

--- assembly-storage/src/AssemblyCalldataDynamic.sol
+++ assembly-storage/src/AssemblyCalldataDynamic.sol
@@ -3,6 +3,19 @@
 pragma solidity ^0.8.30;
 
 contract AssemblyCalldataDynamic {
-    // @TODO: Implement Dynamic calldata management logic
-
+    function firstElement(
+        uint256[] calldata /**
+                            */
+    )
+        external
+        pure
+        returns (uint256)
+    {
+        assembly {
+            let offset := calldataload(0x04)
+            // 0x04 + offset + 0x20 -> data
+            mstore(0x00, calldataload(add(0x024, offset)))
+            return(0x00, 0x20)
+        }
     }
+}
--- assembly-storage/test/AssemblyCalldataDynamicTest.t.sol
+++ assembly-storage/test/AssemblyCalldataDynamicTest.t.sol
@@ -13,7 +13,35 @@ contract AssemblyCalldataDynamicTest is Test {
         assemblyCalldataDynamic = new AssemblyCalldataDynamic();
     }
 
-    function test_calldataDynamic_usecase1() public {
-        // @TODO: Test calldata dynamic contract...
+    function test_calldataDynamic_usecase1() public view {
+        uint256[] memory numbers = new uint256[](3);
+        numbers[0] = 123;
+        numbers[1] = 456;
+        numbers[2] = 789;
+        uint256 firstElement = assemblyCalldataDynamic.firstElement(numbers);
+        assertEq(firstElement, 123);
+    }
+
+    function test_calldataDynamic_usecase2() public view {
+        uint256[] memory numbers = new uint256[](0);
+        uint256 firstElement = assemblyCalldataDynamic.firstElement(numbers);
+        assertEq(firstElement, 0);
+    }
+
+    function test_calldataDynamic_usecase3() public view {
+        uint256[] memory numbers = new uint256[](1);
+        numbers[0] = type(uint256).max;
+        uint256 firstElement = assemblyCalldataDynamic.firstElement(numbers);
+        assertEq(firstElement, type(uint256).max);
+    }
+
+    function test_calldataDynamicFuzz(uint256 firstElement) public view {
+        vm.assume(firstElement < type(uint256).max - 20);
+        uint256[] memory numbers = new uint256[](3);
+        numbers[0] = firstElement;
+        numbers[1] = firstElement + 10;
+        numbers[2] = firstElement + 20;
+        uint256 firstElementStored = assemblyCalldataDynamic.firstElement(numbers);
+        assertEq(firstElementStored, firstElement);
     }
 }
